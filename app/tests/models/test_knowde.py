3878d355602b6387ccccb9cddea317629ef29c75
Shogo Goto <gotoadmn0605@gmail.com>
2022-04-06 20:20:42 +0900

    add knowde tests halfway

---
+++ app/tests/models/test_knowde.py
@@ -0,0 +1,52 @@
+import pytest
+
+from models.core.description import Description
+from models.core.defenition import Defenition
+from models.core import Knowde, KnowdeLink, KnowdeGraph
+
+
+def test_create_with_sorce_link():
+    k1 = Knowde(id="k1", name="name1")
+    k2 = Knowde(id="k2", name="name2", sources=[k1])
+    assert k2.links == set([KnowdeLink(k1.id, k2.id)])
+    k3 = Knowde(id="k3", name="name3", sources=[k1, k2])
+    assert k3.links == set([
+        KnowdeLink(k1.id, k3.id),
+        KnowdeLink(k2.id, k3.id)
+    ])
+
+def test_duplicate_link():
+    k1 = Knowde(id="k1", name="name1")
+    k2 = Knowde(id="k2", name="name2", sources=[k1])
+    k2.linkFrom(k1)
+    assert k2.links == set([KnowdeLink(k1.id, k2.id)])
+
+
+def test_raise_link_src_eq_dest():
+    with pytest.raises(ValueError):
+        KnowdeLink(1, 1)
+
+
+def test_graph():
+    print("####################### test_graph")
+    k1 = Knowde(id="k1", name="name1")
+    k2 = Knowde(id="k2", name="name2", sources=[k1])
+    k3 = Knowde(id="k3", name="name3", sources=[k1, k2])
+    g = KnowdeGraph([k1, k2, k3])
+    # Knowde(id="k1", **{"name": "xxx"})
+    # print(g.nodes)
+    # print(g.links)
+    # g.nodes == [k1, k2, k3]
+    # g.edges == [("k1", "k2"), ("k1", "k3"), ("k2", "k3")]
+# def test_resolve_desctiption():
+#     id = "ID"
+#     desc = Description("xxx[[{}]]xxx", id)
+#     dfn = Defenition(**{id: "NAME"})
+#
+#     d = {
+#         "description": "xxx[[ID]]xxx",
+#         "defenition": {
+#             "ID": "NAME"
+#         }
+#     }
+#     # print(desc.value)
---
+++ app/tests/repository/test_knowde_repository.py
@@ -0,0 +1,46 @@
+import pytest
+
+from repository import KnowdeRepository
+
+# from models.space import Space, SpaceRoot
+from models.core import Knowde
+from models.core import KnowdeGraph
+
+from repository.errors import AlreadyExistsError, NotFoundError
+
+# MATCH (n) DETACH DELETE n
+
+# userId = "space_owner"
+repo = KnowdeRepository()
+
+
+@pytest.fixture(scope="module")
+def fn():
+    pass
+    # cred = Credential(userId, PlainPassword("password"))
+    # if cRepo.findById(userId) is None:
+    #     cRepo.create(cred)
+    # yield
+    # if cRepo.findById(userId) is not None:
+    #     cRepo.delete(cred)
+
+# def test_create(fn):
+#     k = Knowde(name="k1", id="k1")
+#     k = repo.create(k)
+#     found = repo.findById("k1")
+#     assert k == found
+#     repo.delete(k.id)
+#     # k2 = Knowde("k2", id="k2")
+#     # repo.createDestination(k.id, k2)
+#     # k3 = Knowde("k3", id="k3")
+#     # repo.createSource(k.id, k3)
+#
+def test_batch_create_and_delete(fn):
+    k1 = Knowde(name="k1", id="k1")
+    k2 = Knowde(name="k2", id="k2", sources=[k1])
+    k3 = Knowde(name="k3", id="k3", sources=[k1])
+    k4 = Knowde(name="k4", id="k4", sources=[k1])
+    k5 = Knowde(name="k5", id="k5", sources=[k1])
+    g = KnowdeGraph([k1, k2, k3, k4, k5])
+    repo.createBatch(g)
+    # repo.deleteBatch(g)
